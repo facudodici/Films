3afe3d30f9418ad5eaa4912a8c916d09c8f193b2
facudodici <facundomartino96@outlook.com>
2024-07-18 15:19:51 -0300

    cambios

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,8 +1,20 @@
-module.exports = {
-    entry: "./scripts/index.js",
+const path = require('path');
 
-    output: {
-        path: __dirname + "/public",
-        filename: "bundle.js",
-    },
-}
+module.exports = {
+  entry: './scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      // tus loaders aquí
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
+  }
+};
