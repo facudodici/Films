da0764282fc2eed91db88831b9263fc03e8188fe
facudodici <facundomartino96@outlook.com>
2024-07-19 17:26:38 -0300

    First Commit

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-
 
 const dbCon = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        });
+        await mongoose.connect("mongodb+srv://facundomartinodev:aUniogQIdMWRHa4S@cluster0.usagcyd.mongodb.net/PT20A?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Conexión exitosa a la base de datos.");
     } catch (error) {
         console.error("Error al conectar a la base de datos:", error);
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,33 +1,8 @@
-const path = require('path');
-
 module.exports = {
-  entry: './scripts/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'), // Debe ser 'public' dentro del directorio 'front'
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true
-  }
-};
+    entry: "./scripts/index.js",
 
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js",
+    },
+}
