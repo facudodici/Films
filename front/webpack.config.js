46750958476d4997f8a697eb2d6f0ce3339da19e
facudodici <facundomartino96@outlook.com>
2024-07-22 15:29:40 -0300

    all pages integrated in public

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,8 +1,20 @@
-module.exports = {
-    entry: "./scripts/index.js",
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
-    output: {
-        path: __dirname + "/public",
-        filename: "bundle.js",
-    },
-}
+module.exports = {
+  entry: './scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'index.html', to: '' },
+        { from: 'pages/*.html', to: '' },
+        { from: 'styles/*.css', to: '' },
+      ],
+    }),
+  ],
+  mode: 'production',
+};
