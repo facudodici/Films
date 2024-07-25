1d276b57e38107648954fe425b67993297029f70
facudodici <facundomartino96@outlook.com>
2024-07-25 15:43:44 -0300

    first commit

--- back/src/server.js
+++ back/src/server.js
@@ -2,24 +2,13 @@ const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
-const path = require("path");
 
-const app = express(); // app contiene el servidor y lo exporta
+const app = express(); //app contiene el servidor y lo exporta
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-// Configurar para servir archivos estáticos desde el front-end
-app.use(express.static(path.join(__dirname, '../../front/public')));
-
-// Usar el router para las API
 app.use(router); // cuando app recibe la solicitud la envia a router
 
-// Ruta para manejar todas las demás peticiones y devolver el archivo HTML principal
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../front/index.html'));
-});
-
 module.exports = app;
-
--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,21 +1,8 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 module.exports = {
-  mode: 'production',
-  entry: './scripts/index.js', // Ajusta la entrada según tu estructura
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: 'public' }, // Copia archivos estáticos
-      ],
-    }),
-  ],
-  resolve: {
-    extensions: ['.js'],
-  },
-};
+    entry: "./scripts/index.js",
+
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js",
+    },
+}
